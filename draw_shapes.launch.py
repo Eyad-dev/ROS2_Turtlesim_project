9d716ff92ada973b02f792ebc00b42462f1f3f14
Eyad-dev <67559132+Eyad-dev@users.noreply.github.com>
2025-09-28 21:03:21 +0300

    Create draw_shapes.launch.py

---
+++ draw_shapes.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="ros2_project",
+            executable="shape_node",
+            name="shape_node",
+            output="screen"
+        ),
+        Node(
+            package="ros2_project",
+            executable="turtle_commander",
+            name="turtle_commander",
+            output="screen"
+        )
+    ])
